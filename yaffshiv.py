73ec2bd00be913f7f3371ce882d5b3a4f63ea408
eve <devttys0>
2016-01-01 20:52:21 -0500

    Added code comments regarding auto-configuration detection

--- yaffshiv.py
+++ yaffshiv.py
@@ -36,6 +36,19 @@ class YAFFSConfig(object):
     Container class for storing global configuration data.
     '''
 
+    # These are signatures that identify the start of a spare data section,
+    # and hence, the end of a page. If they can be identified, then we can
+    # determine the page size. Take the following hexdump for example:
+    #
+    # 00000800  00 10 00 00 01 01 00 00  00 00 00 00 ff ff ff ff  |................|
+    # 00000810  03 00 00 00 01 01 00 00  ff ff 62 61 72 00 00 00  |..........bar...|
+    # 00000820  00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00  |................|
+    #
+    # The page ends (and spare data begins) at offset 0x800; note that it starts
+    # with the bytes 0x00100000. These are reliable, and would be byte swapped for
+    # a big endian target. Further, if ECC not used, there would be two additional
+    # bytes (0xFFFF) in front of the 0x00100000, so these signatures can also be
+    # used to detect if ECC is used or not.
     SPARE_START_BIG_ENDIAN_ECC = b"\x00\x00\x10\x00"
     SPARE_START_BIG_ENDIAN_NO_ECC = b"\xFF\xFF\x00\x00\x10\x00"
     SPARE_START_LITTLE_ENDIAN_ECC = b"\x00\x10\x00\x00"
@@ -66,19 +79,48 @@ class YAFFSConfig(object):
             sys.stdout.write("    Endianess: %s\n\n" % self.endianess)
 
     def _auto_detect_settings(self):
+        '''
+        This method attempts to identify the page size, spare size, and ECC configuration
+        for the provided sample data. There various methods of doing this, but here we
+        rely on signature based detection. The other method I've seen used is to see if
+        the file is an even multiple of the page size plus the spare size. This method
+        usually assumes that the spare size is 1/32nd of the page size (it doesn't have
+        to be), and also assumes that there is no trailing data on the end of the file
+        system (there very well may be if it's been pulled from a firmware update or a
+        live system).
+
+        The signature method works even if assumptions about the relationship between
+        page size and spare size are violated (in practice they are), and also if we are
+        fed a file that has trailing garbage data. It also allows us to detect the ECC
+        configuration, which is important if you want your YAFFS parsing to actually work.
+        '''
+
+        # Some tools assume that the spare size is 1/32nd of the page size.
+        # For example, if your page size is 4096, then your spare size must be 128.
+        # While this is the default for mkyaffs, you can mix and match, and in
+        # practice, that is exactly what is seen in the wild.
+        #
+        # Thus, we keep a list of valid page sizes and spare sizes, but there
+        # is no restriction on their pairing.
         valid_page_sizes = [512, 1024, 2048, 4096, 8192, 16384, -1]
         valid_spare_sizes = []
 
+        # Valid spare size list is built dynamically based on the valid_page_sizes list
         for page_size in valid_page_sizes:
             if page_size != -1:
                 valid_spare_sizes.append(page_size / 32)
 
-        # Spare data should start at the end of the page; if we can identify it,
-        # then we know the page size used.
+        # Spare data should start at the end of the page. Assuming that the page starts
+        # at the beginning of the data blob we're working with (if it doesn't, nothing
+        # is going to work correctly anyway), if we can identify where the spare data starts
+        # then we know the page size.
         for page_size in valid_page_sizes:
+
             if page_size == -1:
                 raise Exception("Auto-detection failed: Could not locate start of spare data section.")
 
+            # Matching the spare data signatures not only tells us the page size, but also
+            # endianess and ECC layout as well!
             if self.sample_data[page_size:].startswith(self.SPARE_START_LITTLE_ENDIAN_ECC):
                 self.page_size = page_size
                 self.ecclayout = True
@@ -100,19 +142,38 @@ class YAFFSConfig(object):
                 self.endianess = YAFFS.BIG_ENDIAN
                 break
 
+        # Now to try and identify the spare data size...
         try:
+            # If not using the ECC layout, there are 2 extra bytes at the beginning of the
+            # spare data block. Ignore them.
             if not self.ecclayout:
                 offset = 6
             else:
                 offset = 4
 
+            # The spare data signature is built dynamically, as there are repeating data patterns
+            # that we can match on to find where the spare data ends. Take this hexdump for example:
+            #
+            # 00000800  00 10 00 00 01 01 00 00  00 00 00 00 ff ff ff ff  |................|
+            # 00000810  03 00 00 00 01 01 00 00  ff ff 62 61 72 00 00 00  |..........bar...|
+            # 00000820  00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00  |................|
+            #
+            # The spare data starts at offset 0x800 and is 16 bytes in size. The next page data then
+            # starts at offset 0x810. Not that the four bytes at 0x804 (in the spare data section) and
+            # the four bytes at 0x814 (in the next page data section) are identical; also, the
+            # four bytes in the page data are always followed by 0xFFFF, as those are the unused name
+            # checksum bytes.
+            #
+            # Thus, the signature for identifying the next page section (and hence, the end of the
+            # spare data section) becomes: [4 bytes from offset 0x804] + 0xFFFF
             spare_sig = self.sample_data[self.page_size+offset:self.page_size+offset+4] + b"\xFF\xFF"
 
+            # Spare section ends 4 bytes before the spare_sig signature
             self.spare_size = self.sample_data[self.page_size:].index(spare_sig) - 4
         except Exception as e:
             raise Exception("Auto-detection failed: Could not locate end of spare data section.")
 
-        # Sanity check
+        # Sanity check the spare size, make sure it looks legit
         if self.spare_size not in valid_spare_sizes:
             raise Exception("Auto-detection failed: Detected an unlikely spare size: %d" % self.spare_size)
 
@@ -222,6 +283,7 @@ class YAFFSSpare(YAFFS):
 class YAFFSEntry(YAFFS):
     '''
     Parses and stores information from each YAFFS object entry data structure.
+    TODO: Implement as a ctypes Structure class?
     '''
 
     def __init__(self, data, spare, config):
@@ -559,6 +621,7 @@ if __name__ == "__main__":
         config.preserve_owner = preserve_owner
 
     fs = YAFFSExtractor(data, config)
+
     sys.stdout.write("Parsing YAFFS objects...\n")
     obj_count = fs.parse()
     sys.stdout.write("Parsed %d objects\n" % obj_count)
